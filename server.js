629a5f213cdc902adebc87fa7a84da4c4552e453
navarrowebapi <navarro.fabio@gmail.com>
2019-02-21 23:58:53 -0300

    post

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+var mongoose = require('mongoose');
+var Produto = require('./app/models/product');
+
+//PERSISTÊNCIA
+mongoose.connect('mongodb://localhost/bdCrud');
+
+
 //Configuração do server para usar body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,6 +30,22 @@ router.use(function (req, res, next) {
 
 router.get('/',
     (req, res) => res.json({'message': 'rota teste OK'}));
+
+router.route('/produtos')
+    .post(function(req, res){
+        var produto = new Produto();
+        produto.nome = req.body.nome;
+        produto.preco = req.body.preco;
+        produto.descricao = req.body.descricao;
+
+        produto.save(function(error){
+            if(error)
+                res.send("Erro ao tentar salvar produto" + error);
+
+            res.status(201).json({message:'Produto inserido com sucesso'});   
+        });
+    })
+
 
 //Vincular a aplicacao (app) com o motor de rotas
 app.use('/api', router);
